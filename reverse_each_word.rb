6a752852963ab8fef446c65728337eae1421804b
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-29 22:31:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
 def reverse_each_word(sentence)
    sentence.split.collect {|word| word.reverse}.join(" ")
  end
+ # describe #reverse_each_word do
+#   sentence=Hello there, and how are you?
+#   it 'reverses all the words in a string without reversing the order of the words' do
+#     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
