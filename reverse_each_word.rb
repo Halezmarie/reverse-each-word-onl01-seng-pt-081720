8c804fb89be77f59f3773882ff6d09013338f914
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-29 22:34:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,3 @@
-def reverse_each_word(string)
-  Array.new 
-  loop = string.length
-  word = ''
-  while loop > 0 
-  loop -= 1
-  word += string[loop]
-end
-end
-
-def sentence1
-  puts "Hello there, and how are you?"
-end
-
-def sentence2
-  puts "Hi again, just making sure it's reversed!"
-end
-
 
 #reverse_each_word("Hello there, and how are you?")
   #=> "olleH ,ereht dna woh era ?uoy"
